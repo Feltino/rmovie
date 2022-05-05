21300fea079129074742d86508bf4b87500b3460
Alexandr <alexandr@Alexandrs-Mac-mini.local>
2022-05-05 12:08:33 +0300

    урок 19 практика ч.1

---
+++ js/main.js
@@ -0,0 +1,20 @@
+"use strict"
+
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+let personalMovieDB = {
+
+  count: numberOfFilms,
+  movie: {},
+  actors: {},
+  genres: [],
+  privat: false
+
+};
+
+let firstMovie = prompt('Один из последних просмотренных фильмов?', '');
+let firstMoviePoint = +prompt('На сколько оцените его?', '');
+let secondMovie = prompt('Один из последних просмотренных фильмов?', '');
+let secondMoviePoint = +prompt('На сколько оцените его?', '');
+
+personalMovieDB.movie[firstMovie] = firstMoviePoint;
