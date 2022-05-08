e70093243491605f33ffd4793118e88688871e1a
Alexandr <alexandr@Alexandrs-Mac-mini.local>
2022-05-08 14:26:57 +0300

    Практика, ч2. Применяем условия и циклы

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
@@ -12,10 +12,34 @@ let personalMovieDB = {
 
 };
 
-let firstMovie = prompt('Один из последних просмотренных фильмов?', '');
-let firstMoviePoint = +prompt('На сколько оцените его?', '');
-let secondMovie = prompt('Один из последних просмотренных фильмов?', '');
-let secondMoviePoint = +prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+  let setMovie = prompt('Один из последних просмотренных фильмов?', '');
+  // while (setMovie === null || setMovie.length === 0 || setMovie.length >= 50) {
+  //   setMovie = prompt('Один из последних просмотренных фильмов?', '');
+  // }
 
-personalMovieDB.movie[firstMovie] = firstMoviePoint;
-personalMovieDB.movie[secondMovie] = secondMoviePoint;
+  let setRewies = +prompt('На сколько оцените его?', '');
+
+  if (setMovie != null && setRewies != null && setMovie != '' && setMovie != '' && setMovie.length < 50) {
+    personalMovieDB.movie[setMovie] = setRewies;
+  } else {
+    i--;
+  }
+  // personalMovieDB.movie[setMovie] = setRewies;
+}
+
+if (personalMovieDB.count < 10) {
+  alert('Просмотренно довольно мало фильмов')
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  alert('Вы классический зритель')
+} else if (personalMovieDB.count >= 30) {
+  alert('Вы киноман');
+}
+
+console.log(personalMovieDB);
+
+// let secondMovie = prompt('Один из последних просмотренных фильмов?', '');
+// let secondMoviePoint = +prompt('На сколько оцените его?', '');
+
+
+// personalMovieDB.movie[secondMovie] = secondMoviePoint;
